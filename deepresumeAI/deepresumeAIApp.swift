d1b0b302761589286a1f98aac06a56cff3589b7f
PantherValley13 <Churchdarius@gmail.com>
2025-05-03 23:27:52 -0400

    Initial Commit

---
+++ deepresumeAI/deepresumeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  deepresumeAIApp.swift
+//  deepresumeAI
+//
+//  Created by Darius Church on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct deepresumeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ deepresumeAITests/deepresumeAITests.swift
@@ -0,0 +1,17 @@
+//
+//  deepresumeAITests.swift
+//  deepresumeAITests
+//
+//  Created by Darius Church on 5/3/25.
+//
+
+import Testing
+@testable import deepresumeAI
+
+struct deepresumeAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
